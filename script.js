2a4857f05d2d02145a99a6fce0ecc98335795ac7
Kumar Sambhav <krsambhav16@gmail.com>
2020-08-22 16:36:46 +0530

    ...

---
+++ script.js
@@ -0,0 +1,51 @@
+let inputBox = $("#inputBox");
+let url = "https://saavn.glitch.me/";
+let outputContainer = $('#songContainer');
+
+inputBox.on("keypress", (e) => {
+    if (e.which == 13) {
+        songSearch(e);
+        outputContainer.html('<img src="./loading.gif">');
+    }
+});
+
+function songSearch(e) {
+    let query = inputBox.val();
+    query = query.replace(/\s/g, "+");
+    let finURL = url + query;
+    getData(finURL);
+}
+
+function getData(url) {
+    axios
+        .get(url)
+        .then((resp) => manipulateData(resp.data))
+        .catch((err) => console.log(err));
+}
+
+function manipulateData(data) {
+    let output = '';
+    data.map((song) => {
+        let songName = song.songName;
+        let singers = song.singers;
+        let image = song.image;
+        let url = song.url;
+        let duration = song.duration;
+        output += `<div class="card text-center m-5 bg-dark text-white border-0" style="width: 15rem;">
+        <img src="${image}" style="width: 15rem;">
+        <div class="card-body">
+            <h3 class="card-title text-center">
+                ${songName}
+            </h3>
+            <div class="card-text">
+                Singers: <span id="singer">${singers}</span>
+                <br>
+                Duration: <span id="duration">${duration}</span>
+            </div>
+            <br>
+            <a href="${url}"><button class="btn btn-primary">Download</button></a>
+        </div>
+    </div>`;
+    });
+    outputContainer.html(output);
+}
